42fe68126050fd26a7f16a58564c1670d9d8771a
nmandel <nrmandel@ucdavis.edu>
2014-11-24 16:57:55 -0800

    implements get request for links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
+.factory('Links', function() {
+  return {};
+})
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = Links;
+  $scope.getLinks = function() {
+    $http.get('/api/links')
+      .success(function(data) {
+        $scope.data.links = data;
+        console.log(data);
+      })
+    // console.log(Links);
+  };
+  $scope.getLinks();
 });
