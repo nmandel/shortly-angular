5e5d3d6f92f5405d4e24021a2aae2bf1354a77a6
nmandel <nrmandel@ucdavis.edu>
2014-11-24 17:21:47 -0800

    implements addlink functionality

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,12 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $http, $location) {
+  $scope.link = {};
+  $scope.addLink = function(str) {
+    $http.post('/api/links', {url: str})
+      .success(function(data) {
+        console.log(data);
+      })
+  };
+
 });
